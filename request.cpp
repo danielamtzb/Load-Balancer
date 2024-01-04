b66930357fa4469383ad24d4e20ba3628b2e8aaa
danielamtzb <daniela.mtzbanda@gmail.com>
2024-01-04 10:21:57 +0800

    requests and server classes made

---
+++ request.cpp
@@ -0,0 +1,11 @@
+
+#include <string>
+#define REQUEST.CPP
+
+struct request{
+
+    std::string source;
+    std::string dest;
+    int time;
+
+};
---
+++ webserver.cpp
@@ -0,0 +1,34 @@
+
+#ifndef REQUEST_CPP
+#include "request.cpp"
+#endif
+
+class webserver{
+
+  private:
+    request req;
+    int rinTime;
+    char serverName;
+
+  public:
+
+    //Constructor
+    webserver(): rinTime(0), serverName(' ') {}
+    webserver(char n): rinTime(0), serverName(n) {}
+
+    //Adding a request to your server
+    void addRequest(request r, int currTime){
+        req = r;
+        rinTime = currTime;
+    }
+
+    //Getter functions
+    request getRequest(){ return req; }
+    char getName(){ return serverName; }
+
+    bool isRequestFinish(int currTime){
+        if(currTime > (rinTime + req.time)){ return true; }
+        else{ return false; }
+    }
+
+};
